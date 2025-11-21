e7a81a58f9f07f99d013cda230435da34648dc03
brianhe8 <brian.he@ucla.edu>
2025-11-21 12:47:49 -0800

    styling to item image and guess cells

--- traderjodle/frontend/src/App.tsx
+++ traderjodle/frontend/src/App.tsx
@@ -1,21 +1,20 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Guesses from "./components/Guesses";
-import LoadImage from "./assets/loading.jpg";
+import { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Guesses from './components/Guesses';
+import LoadImage from './assets/loading.jpg';
 
 interface RealItem {
-    // prob useless?
     id: number;
     item_name: string;
     item_price: string;
     item_image: string;
 }
 export default function Game() {
-    const [itemName, setItemName] = useState<string>("");
-    const [itemSolution, setItemSolution] = useState<string>("");
-    const [itemImage, setItemImage] = useState<string>("");
+    const [itemName, setItemName] = useState<string>('');
+    const [itemSolution, setItemSolution] = useState<string>('');
+    const [itemImage, setItemImage] = useState<string>('');
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [guessesSubmitted, setGuessesSubmitted] = useState<number>(1);
     const [hasWon, setHasWon] = useState<boolean>(false);
@@ -33,7 +32,7 @@ export default function Game() {
         try {
             fetchData();
         } catch {
-            console.error("Was not able to fetch data from database.");
+            console.error('Was not able to fetch data from database.');
         }
     }, []);
 
@@ -41,21 +40,21 @@ export default function Game() {
         currGuessCountUpdate: number,
         hasWonUpdate: boolean
     ) => {
-        console.log("=====Updating Game Info=====");
+        console.log('=====Updating Game Info=====');
         if (!hasWonUpdate) {
             setGuessesSubmitted(currGuessCountUpdate);
         }
         // doesnt set guessesSubmitted until after handleGameInfoUpdate
-        console.log(3, "guessesSubmitted: ", guessesSubmitted);
+        console.log(3, 'guessesSubmitted: ', guessesSubmitted);
         if (guessesSubmitted === 6) {
             setIsGameOver(true);
-            console.log("Game Over!");
+            console.log('Game Over!');
         }
         // console.log("Upper level hasWonTemp: ", hasWonUpdate);
         console.log(4, hasWonUpdate);
         setHasWon((hasWon) => hasWonUpdate);
         console.log(5, hasWon);
-        console.log("============================");
+        console.log('============================');
     };
     return (
         <>
@@ -74,25 +73,25 @@ export default function Game() {
                             className="item-image"
                         />
                     </div>
-                    <div className="image-name-container">
-                        <p>{isLoading ? "" : itemName}</p>
+                    <div className="image-item-name-container">
+                        <p>{isLoading ? '' : itemName}</p>
                     </div>
                 </div>
                 <div className="game-stats">
                     <p>
                         {guessesSubmitted >= 7
-                            ? "Guess 6/6"
-                            : "Guess " + guessesSubmitted + "/6"}
+                            ? 'Guess 6/6'
+                            : 'Guess ' + guessesSubmitted + '/6'}
                     </p>
                     <p>
                         {hasWon
-                            ? "You win! The answer is: " + itemSolution
-                            : ""}
+                            ? 'You win! The answer is: ' + itemSolution
+                            : ''}
                     </p>
                     <p>
                         {isGameOver && !hasWon
                             ? "You'll get it next time!"
-                            : ""}
+                            : ''}
                     </p>
                 </div>
                 <div className="guesses-container">
@@ -110,7 +109,7 @@ export default function Game() {
 }
 
 async function getData(): Promise<RealItem[]> {
-    const response = await fetch("http://localhost:3000/items");
+    const response = await fetch('http://localhost:3000/items');
     const data = await response.json();
     return data;
 }
